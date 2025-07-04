110f52959c64c155a7351ff753e7cb7250a14f80
analuisa-unifesp <ana.batista@unifesp.br>
2025-07-04 14:47:42 -0300

    Create 1005.c

---
+++ 1005.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    float notaA, notaB;
+    double media;
+    
+    scanf("%f", &notaA);
+    scanf("%f", &notaB);
+    
+    media = (notaA*3.5 + notaB*7.5)/11;
+    
+    printf("MEDIA = %.5lf\n", media);
+    
+    return 0;
+}
